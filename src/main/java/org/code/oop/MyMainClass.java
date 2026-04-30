26289bc12171d3948f373afbf5902520fd5d8eaa
waiting <yexiaonanair@outlook.com>
2026-04-30 10:43:39 +0800

    fix: fix pom.xml file name error

--- src/main/java/org/code/oop/MyMainClass.java
+++ src/main/java/org/code/oop/MyMainClass.java
@@ -1,18 +1,18 @@
 package org.code.oop;
 
-class Animal {
+class PolyAnimal {
     public void animalSound() {
         System.out.println("The animal makes a sound");
     }
 }
 
-class Pig extends Animal {
+class PolyPig extends PolyAnimal {
     public void animalSound() {
         System.out.println("The pig says: wee wee");
     }
 }
 
-class Dog extends Animal {
+class PolyDog extends PolyAnimal {
     public void animalSound() {
         System.out.println("The dog says: bow wow");
     }
@@ -20,9 +20,9 @@ class Dog extends Animal {
 
 class MyMainClass {
     public static void main(String[] args) {
-        Animal myAnimal = new Animal();  // 创建一个 Animal 对象
-        Animal myPig = new Pig();  // 创建 Pig 对象
-        Animal myDog = new Dog();  // 创建一个 Dog 对象
+        PolyAnimal myAnimal = new PolyAnimal();
+        PolyAnimal myPig = new PolyPig();
+        PolyAnimal myDog = new PolyDog();
         myAnimal.animalSound();
         myPig.animalSound();
         myDog.animalSound();
