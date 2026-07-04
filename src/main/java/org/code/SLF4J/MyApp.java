c943f4681efb76d39f2dc20e313d03101ce99714
waiting <yexiaonanair@outlook.com>
2026-07-04 18:36:20 +0800

    feat: add SLF4J

---
+++ src/main/java/org/code/SLF4J/MyApp.java
@@ -0,0 +1,21 @@
+package org.code.SLF4J;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyApp {
+    // 获取 Logger 实例
+    private static final Logger logger = LoggerFactory.getLogger(MyApp.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started");
+
+        try {
+            // 业务逻辑
+        } catch (Exception e) {
+            logger.error("An error occurred", e);
+        }
+
+        logger.info("Application finished");
+    }
+}
